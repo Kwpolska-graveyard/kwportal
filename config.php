5add35bb1f3d1bbe8ac55e9f083d60b4b268c1ad
Kwpolska <kwpolska@gmail.com>
2011-01-24 16:43:59 +0100

    Updating the savant3 code.

--- config.php
+++ config.php
@@ -3,12 +3,13 @@
 //Copyright Kwpolska 2010-2011.
 
 /// KWPORTAL SETTINGS ///
-$configured = false; //IMPORTANT: change it to true.
+$configured = false; //IMPORTANT: change it to true.i
 $markdown = true; //if you REALLY don't want to use Markdown and SmartyPants, use this.
+
 /// USERS SETTINGS ///
 $users = array(1 =>
-   array('uname' => 'USERNAME', 'passwd' => 'A SHA512 PASSWORD'),
-);
+      array('uname' => 'USERNAME', 'passwd' => 'A SHA512 PASSWORD'),
+      );
 $saltbase = 'kwportalsawesome'; //salt base, up to 16 characters, longer ones will be trimmed
 
 /// DATABASE SETTINGS ///
@@ -22,10 +23,13 @@ $dbdsn = 'mysql:host='.$dbhst.';dbname='.$dbnme; // change mysql if needed
 /// DYNAMIC SETTINGS ///
 $salt = '$6$rounds=5000$'.$saltbase.'$';
 function savant($template) {
-	include_once 'Savant3.php';
-	$tpl = new Savant3();
-	$tpl->title = $title;
-	$tpl->content = $content;
-	$tpl->display($template.'.tpl.php');
+   global $content;
+   include_once 'Savant3.php';
+   $tpl = new Savant3();
+   $tpl->setEscape();
+   $tpl->title = KP_NME;
+   $tpl->content = $content;
+   $tpl->display($template.'.tpl.php');
 }
+
 ?>
