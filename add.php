7c1cc257c941a17ad6ce0472cf4c9c63eb81402a
Kwpolska <kwpolska@gmail.com>
2011-01-28 20:01:26 +0100

    fixed the add.php

--- add.php
+++ add.php
@@ -10,41 +10,40 @@ echo '<h1>KwPortal</h1>
 <li><a href="./logout.php">Logout</a></li>
 </ul>';
 if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    if($markdown = true) {
-        include_once './smartypants.php';
-        include_once './markdown.php';
-        $pcontent = SmartyPants(Markdown($_POST['cnt']));
-    }
-    try
-    {
-        $pdo = new PDO($dbdsn, $dbusr, $dbpwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-        $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+   if($markdown = true) {
+      include_once './smartypants.php';
+      include_once './markdown.php';
+      $pcontent = SmartyPants(Markdown($_POST['cnt']));
+   }
+   try
+   {
+      $pdo = new PDO($dbdsn, $dbusr, $dbpwd, array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+      $pdo -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        $stmt = $pdo -> prepare('INSERT INTO `'.$dbtbl.'` (`content`, `title`, `timestamp`) VALUES(
-                    :content,
-                    :title,
-                    :timestamp)');	// 1
+      $stmt = $pdo -> prepare('INSERT INTO `'.$dbtbl.'` (`content`, `title`, `timestamp`) VALUES(
+               :content,
+               :title,
+               :timestamp)');	// 1
 
-        $stmt -> bindValue(':title', $_POST['title'], PDO::PARAM_STR);
-        $stmt -> bindValue(':timestamp', time(), PDO::PARAM_STR);
-        $stmt -> bindValue(':content', $pcontent, PDO::PARAM_STR);
+      $stmt -> bindValue(':title', $_POST['title'], PDO::PARAM_STR);
+      $stmt -> bindValue(':timestamp', time(), PDO::PARAM_STR);
+      $stmt -> bindValue(':content', $pcontent, PDO::PARAM_STR);
 
-        $ilosc = $stmt -> execute();
+      $ilosc = $stmt -> execute();
 
-        if($ilosc = 1)
-        {
-            echo 'Roger that.';
-        }
-        else
-        {
-            echo 'wut?';
-        }
-    }
-    catch(PDOException $e)
-    {
-        die 'ERROR: ' . $e->getMessage();
-    }
-    die '<div class=\"info\">Successfully added.</div>';
+      if($ilosc = 1)
+      {
+         die("<div class=\"info\">The entry was successfully deleted.</div>");
+      }
+      else
+      {
+         echo 'There was an issue with adding the entry.';
+      }
+   }
+   catch(PDOException $e)
+   {
+      die 'ERROR: ' . $e->getMessage();
+   }
 }
 echo '<form method="POST" action="?">
 Title: <input name="title"><br>
