27a424f1a43321ceb174b6e1cef58e2626a3754d
Kwpolska <kwpolska@gmail.com>
2011-07-16 20:47:05 +0200

    useless updates...

--- init.php
+++ init.php
@@ -1,15 +1,37 @@
 <?php
-//KwPortal
-//Copyright Kwpolska 2010.
+//Kw's Session Initialization Script
+//Part of KRU
+/*
+Copyright (C) 2011, Kwpolska.
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+ */
 session_start(); //Start the session
 if (!isset($_SESSION['init'])) {
     session_regenerate_id(); //Regenerate the session ID
-    $_SESSION['init'] = true; //Remember that the session was initialized 
+    $_SESSION['init'] = true; //Remember that the session was initialized
 }
-if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 1800)) {
+if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] >
+                                          1800)) {
     // last request was more than 30 minates ago
     session_destroy();   // destroy session data in storage
     session_unset();     // unset $_SESSION variable for the runtime
 }
-$_SESSION['LAST_ACTIVITY'] = time(); // update last activity time stamp
+$_SESSION['LAST_ACTIVITY'] = time(); // update last activity timestamp
 ?>
